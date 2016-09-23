6e2cb1808a7d058ed015b700b4117204585a45b9
zeven <fangfan1997@126.com>
2016-09-23 12:09:01 +0800

    initial config.php

--- conf/config.php
+++ conf/config.php
@@ -1,10 +1,4 @@
 <?php 
 return array (
-  'installed' => true,
-  'host' => 'localhost',
-  'username' => 'root',
-  'password' => '',
-  'db' => 'zevenblog',
-  'port' => '3306',
-  'charset' => 'utf8',
+  'installed' => false
 );
